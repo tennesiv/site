f64170354f5f3bbf0d189cc7eedf0a284f0fa028
Michil <godof830@gmail.com>
2025-03-09 20:22:55 +0900

    sc

--- js/app.js
+++ js/app.js
@@ -16,3 +16,18 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   });
 });
+
+// Появление секций при скролле
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+document.querySelectorAll('.hidden').forEach(section => {
+  observer.observe(section);
+});
