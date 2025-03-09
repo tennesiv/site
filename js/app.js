894e67015867a3ccd9906ab5747bdc54fe01ccfb
Michil <godof830@gmail.com>
2025-03-09 20:11:26 +0900

    полностью поменял всю структуру

---
+++ js/app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".burger");
+  const navMenu = document.querySelector(".nav-menu");
+
+  burger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+});
